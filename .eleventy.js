e4a3194a5fffc246e6bd8de868ccd8bfc875f439
David Krismer <david.krismer71@gmail.com>
2023-08-18 16:14:52 -0700

    Updated output path to _site

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/images");
 
     return {
-        dir: { input: 'src', output: 'docs' }
+        dir: { input: 'src', output: '_site' }
     };
 };
